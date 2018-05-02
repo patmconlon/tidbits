657fc8b0f8f1a9486807cd7b1661f6706972a35b
Patrick Conlon <pmconlon@west.com>
2018-05-02 12:37:35 -0500

    Added the tabs instead of spaces.

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -10,17 +10,17 @@
 using namespace std;
 
 int main() {
-    int a = 0; //low number
-    int b = 1; //high number
+	int a = 0; //low number
+	int b = 1; //high number
 
-   //Loop till done
-    do {
-        b += a;
-        a = b - a;
+	//Loop till done
+	do {
+		b += a;
+		a = b - a;
 
-      cout << a << "\n";
-    }
-   while( b < 1000000000 );
+		cout << a << "\n";
+	}
+	while( b < 1000000000 );
 
-    return 0;
+	return 0;
 }
